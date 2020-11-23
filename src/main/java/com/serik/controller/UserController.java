d1840bf3a5a38618ae2c0327027629fa79fd31e0
serikbatyrkhanov <Serik3571891>
2020-11-23 14:36:31 -0500

    first commit and push

--- src/main/java/com/serik/controller/UserController.java
+++ src/main/java/com/serik/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.serik.controller;
 
 import com.serik.dto.UserDTO;
+import com.serik.implementation.RoleServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +15,7 @@ public class UserController {
     public String createUser(Model model){
 
         model.addAttribute("user",new UserDTO());
-
+        model.addAttribute("roleList", new RoleServiceImpl().findAll());
 
         return "/user/create";
     }
