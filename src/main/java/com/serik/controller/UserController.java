abcdb051ebe4d7a19783246b646895fc7b382003
serikbatyrkhanov <Serik3571891>
2020-11-24 21:42:03 -0500

    added user update

--- src/main/java/com/serik/controller/UserController.java
+++ src/main/java/com/serik/controller/UserController.java
@@ -1,22 +1,62 @@
 package com.serik.controller;
 
 import com.serik.dto.UserDTO;
-import com.serik.implementation.RoleServiceImpl;
+import com.serik.service.RoleService;
+import com.serik.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    RoleService roleService;
+
+    @Autowired
+    UserService userService;
+
     @GetMapping("/create")
     public String createUser(Model model){
 
         model.addAttribute("user",new UserDTO());
-        model.addAttribute("roleList", new RoleServiceImpl().findAll());
+        model.addAttribute("roles", roleService.findAll());
+        model.addAttribute("users", userService.findAll());
 
+        return "/user/create";
+    }
+
+    @PostMapping("/create")
+    public String insertUser(UserDTO user, Model model){
+
+        userService.save(user);
+        model.addAttribute("user",  new UserDTO());
+        model.addAttribute("roles", roleService.findAll());
+        model.addAttribute("users", userService.findAll());
+        return "/user/create";
+    }
+
+    @GetMapping("/update/{username}")
+    public String editUser(@PathVariable("username") String username, Model model){
+
+        model.addAttribute("user", userService.findById(username));
+        model.addAttribute("users", userService.findAll());
+        model.addAttribute("roles", roleService.findAll());
+        return "user/update";
+    }
+
+    @PostMapping ("/update/{username}")
+    public String updateUser(@PathVariable("username") String username,UserDTO user, Model model){
+
+        userService.update(user);
+        model.addAttribute("user", new UserDTO());
+        model.addAttribute("users", userService.findAll());
+        model.addAttribute("roles", roleService.findAll());
         return "/user/create";
     }
 }
